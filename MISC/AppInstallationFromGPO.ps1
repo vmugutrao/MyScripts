4da4ef2f3954302ca51124f1f3cb19a923b8c5eb
vmugutrao <vishalmugutrao@gmail.com>
2019-08-04 21:28:30 +0530

    commit

--- MISC/AppInstallationFromGPO.ps1
+++ MISC/AppInstallationFromGPO.ps1
@@ -1,11 +1,9 @@
-﻿
-$Check = $null
-$Installpath = "\\mybeast\Fileshare\Common\Deployments\CompassConsole\UAT\Releases\Setup.exe"
+﻿$Installpath = "\\mybeast\Fileshare\Common\Deployments\CompassConsole\UAT\Releases\Setup.exe"
 $RegPath = "HKCU:Software\Microsoft\Windows\CurrentVersion\Uninstall\CompassConsoleUAT"
 $Logfile = "$($env:USERPROFILE)\Desktop\Compassinstall.log"
 
 Add-Content $Logfile "$(Get-Date) : Comppas Console app installation Started !!!" -Force -ErrorAction SilentlyContinue
-$Check
+
 If(Test-Path $RegPath)
     {
     Add-Content $Logfile "$(Get-Date) : Comppas Console app is already installed, No action Taken !! " -Force -ErrorAction SilentlyContinue
@@ -23,5 +21,3 @@ Else
         Add-Content $Logfile "Error: $($Error[0].Exception.InnerException)" -Force -ErrorAction SilentlyContinue
         }
     }
-
-    
